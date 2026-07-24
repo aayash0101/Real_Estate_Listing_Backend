a16c91d2e06b2f9915ae550d34c8c105a0935c44
Aayash <dangaayash@gmail.com>
2026-07-24 13:52:08 +0545

    listing route updated

--- src/listings/listing.routes.ts
+++ src/listings/listing.routes.ts
@@ -5,6 +5,8 @@ import {
   createListing,
   updateListingHandler,
   deleteListingHandler,
+  uploadImages,
+  deleteImage,
 } from "./listing.controller";
 import {
   validateListingQuery,
@@ -12,6 +14,7 @@ import {
   validatePropertyUpdateBody,
 } from "../middleware/validate.middleware";
 import { requireAgent } from "../middleware/auth.middleware";
+import { uploadListingImages } from "../middleware/upload.middleware";
 
 const router = Router();
 
@@ -21,5 +24,8 @@ router.get("/:id", getListingDetail);
 router.post("/", requireAgent, validatePropertyBody, createListing);
 router.patch("/:id", requireAgent, validatePropertyUpdateBody, updateListingHandler);
 router.delete("/:id", requireAgent, deleteListingHandler);
+
+router.post("/:id/images", requireAgent, uploadListingImages.array("images", 10), uploadImages);
+router.delete("/images/:imageId", requireAgent, deleteImage);
 
 export default router;
